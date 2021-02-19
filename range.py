0dd4d2c7ae0eca214226992b993f10c41719822c
Rishit Khandelwal <rishitk@gmail.com>
2021-02-19 12:11:34 +0530

    Added some more functions

--- range.py
+++ range.py
@@ -1,7 +1,14 @@
-def range(start:int,end:int):
+def lrange(start:int,end:int):
     x=start
     res=[]
     while x<end:
         res.append(x)
         x+=1
     return res
+
+def printrange(start:int,end:int):
+    x=start
+    while x<end:
+        print(x)
+        x+=1
+
